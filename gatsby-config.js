3da48eb7770d23a9c8a856a37b9779eae885e931
Noel Mace <contact@noelmace.com>
2019-07-09 01:10:08 +0200

    fix: site metadatas

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Bonita Talks',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
-  },
+      'Talks abstracts & more, with ♡ from Bonitasoft'
+    },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
