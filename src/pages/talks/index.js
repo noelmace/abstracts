c5678ddabc7f7be075a9723ef945ff7b10c1640c
Noel Mace <contact@noelmace.com>
2019-07-17 15:42:28 +0200

    fix: change talks index image

--- src/pages/talks/index.js
+++ src/pages/talks/index.js
@@ -10,7 +10,7 @@ export default class TalksIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/bonitasoft_devices_crop.png')`,
+            backgroundImage: `url('/img/keyboard.jpeg')`,
           }}
         >
           <h1
